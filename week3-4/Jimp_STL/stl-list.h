9b3c7b03489d117086377f483cbef6f91d714e13
Jimp <2632697420@qq.com>
2021-07-24 15:53:08 +0800

    'week3-4'

--- week3-4/Jimp_STL/stl-list.h
+++ week3-4/Jimp_STL/stl-list.h
@@ -87,20 +87,20 @@ struct _List_iterator : public _List_iterator_base {
 inline bidirectional_iterator_tag
 iterator_category(const _List_iterator_base&)
 {
-		return bidirectional_iterator_tag();
+	return bidirectional_iterator_tag();
 }
 
 template <class _Tp, class _Ref, class _Ptr>
 inline _Tp*
 value_type(const _List_iterator<_Tp, _Ref, _Ptr>&)
 {
-		return 0;
+	return 0;
 }
 
 inline ptrdiff_t*
 distance_type(const _List_iterator_base&)
 {
-		return 0;
+	return 0;
 }
 
 template <class _Tp, class _Alloc>
@@ -278,22 +278,9 @@ public:
 		return insert(__position, _Tp());
 	}
 
-	template<class _Integer>
-	void _M_insert_dispatch(iterator __pos, _Integer __n, _Integer __x,
-					   __true_type) {
-		_M_fill_insert(__pos, (size_type)__n, (_Tp)__x);
-	}
-
-	template<class _InputIterator>
-	void _M_insert_dispatch(iterator __pos,
-					   _InputIterator __first, _InputIterator __last,
-					   __false_type);
-
-	template<class _InputIterator>
-	void insert(iterator __pos, _InputIterator __first, _InputIterator __last) {
-		typedef typename _Is_integer<_InputIterator>::_Integral _Integral;
-		_M_insert_dispatch(__pos, __first, __last, _Integral());
-	}
+	void insert(iterator __position,const _Tp* __first, const _Tp* __last);
+	void insert(iterator __position,
+		   const_iterator __first, const_iterator __last);
 
 	void insert(iterator __pos, size_type __n, const _Tp& __x) {
 		_M_fill_insert(__pos, __n, __x);
@@ -328,8 +315,285 @@ public:
 		_Base::clear();
 	}
 
+	void resize(size_type __new_size, const _Tp& __x);
+	void resize(size_type __new_size) {
+		this->resize(__new_size, _Tp());
+	}
 
+	void pop_front() {
+		erase(begin());
+	}
+	void pop_back() {
+		iterator __tmp = end();
+		erase(--__tmp);
+	}
+
+	list(size_type __n, const _Tp& __value,
+		 const allocator_type& __a = allocator_type())
+	: _Base(__a) {
+		insert(begin(), __n, __value);
+	}
+	explicit list(size_type __n)
+	: _Base(allocator_type()) {
+		insert(begin(), __n, _Tp());
+	}
+
+	list(const _Tp* __first, const _Tp* __last,
+		 const allocator_type& __a = allocator_type())
+	: _Base(__a) {
+		this->insert(begin(), __first, __last);
+	}
+	list(const_iterator __first, const_iterator __last,
+		 const allocator_type& __a = allocator_type())
+	: _Base(__a) {
+		this->insert(begin(), __first, __last);
+	}
+
+	list(const list<_Tp, _Alloc>& __x)
+	: _Base(__x.get_allocator()) {
+		insert(begin(), __x.begin(), __x.end());
+	}
+
+	~list();
+
+	list<_Tp, _Alloc>& operator=(const list<_Tp, _Alloc>& __x);
+
+public:
+	void assign(size_type __n, const _Tp& __val) {
+		_M_fill_assign(__n, __val);
+	}
+	void _M_fill_assign(size_type __n, const _Tp& __val);
+
+protected:
+	void transfer(iterator __position, iterator __first, iterator __last) {
+		if(__position != __last) {
+			__last._M_node->_M_prev->_M_next      = __position._M_node;
+			__first._M_node->_M_prev->_M_next     = __last._M_node;
+			__position._M_node->_M_prev->_M_next  = __first._M_node;
+
+			_List_node_base* __tmp      = __position._M_node->_M_prev;
+			__position._M_node->_M_prev = __last._M_node->_M_prev;
+			__last._M_node->_M_prev     = __first._M_node->_M_prev;
+			__first._M_node->_M_prev    = __tmp;
+		}
+	}
+
+public:
+	void splice(iterator __position, list& __x) {
+		if(!__x.empty())
+			this->transfer(__position, __x.begin(), __x.end());
+	}
+	void splice(iterator __position, list&, iterator __i) {
+		iterator  __j = __i;
+		++__j;
+		if(__position == __i || __position == __j)
+			return;
+		this->transfer(__position, __i, __j);
+	}
+	void splice(iterator __position, list&, iterator __first, iterator __last) {
+		if(__first != __last)
+			this->transfer(__position, __first, __last);
+	}
+
+	void remove(const _Tp& __value);
+	void unique();
+	void merge(list& __x);
+	void reverse();
+	void sort();
 };
+
+template <class _Tp, class _Alloc>
+inline bool
+operator==(const list<_Tp, _Alloc>& __x, const list<_Tp, _Alloc>& __y)
+{
+	typedef typename list<_Tp, _Alloc>::const_iterator  const_iterator;
+	const_iterator __end1 = __x.end();
+	const_iterator __end2 = __y.end();
+
+	const_iterator __i1 = __x.begin();
+	const_iterator __i2 = __y.begin();
+	while (__i1 != end1 && __i2 != __end2 && *__i1 == *__i2) {
+		++__i1;
+		++__i2;
+	}
+	return __i1 == __end1 && __i2 == __end2;
+}
+
+template <class _Tp, class _Alloc>
+inline bool operator<(const list<_Tp, _Alloc>& __x,
+		              const list<_Tp, _Alloc>& __y)
+{
+	return  lexicographical_compare(__x.begin(), __x.end(),
+								    __y.begin(), __y.end());
+}
+
+template <class _Tp, class _Alloc>
+void
+list<_Tp, _Alloc>::insert(iterator __position,
+						  const _Tp* __first, const _Tp* __last)
+{
+	for( ; __first != __last; ++__first)
+		insert(__position, *__first);
+}
+
+template <class _Tp, class _Alloc>
+void
+list<_Tp, _Alloc>::insert(iterator __position,
+						  const_iterator __first, const_iterator __last)
+{
+	for( ; __first != __last; ++__first)
+		insert(__position, *__first);
+}
+
+template <class _Tp, class _Alloc>
+void
+list<_Tp, _Alloc>::_M_fill_insert(iterator __position,
+								  size_type __n, const _Tp& __x)
+{
+	for( ; __n > 0; --__n)
+		insert(__position, __x);
+}
+
+template <class _Tp, class _Alloc>
+typename list<_Tp, _Alloc>::iterator list<_Tp, _Alloc>::erase(iterator __first,
+						                                      iterator __last)
+{
+	while (__first != __last)
+		erase(__first++);
+	return __last;
+}
+
+template <class _Tp, class _Alloc>
+void list<_Tp, _Alloc>::resize(size_type __new_size, const _Tp& __x)
+{
+	iterator __i = begin();
+	size_type __len = 0;
+	for( ; __i != end() && __len < __new_size; ++__i, ++__len)
+		;
+	if(__len == __new_size)
+		erase(__i, end());
+	else
+		insert(end(), __new_size - __len, __x);
+}
+
+template <class _Tp, class _Alloc>
+list<_Tp, _Alloc>& list<_Tp, _Alloc>::operator=(const list<_Tp, _Alloc>& __x)
+{
+	if(this != &__x) {
+		iterator __first1 = begin();
+		iterator __last1 = end();
+		const_iterator __first2 = __x.begin();
+		const_iterator __last2 = __x.end();
+		while (__first1 != __last1 && __first2 != __last2)
+			*__first1++ = *__first2++;
+		if(__first2 == __last2)
+			erase(__first1, __last1);
+		else
+			insert(__last1, __first2, __last2);
+	}
+}
+
+template <class _Tp, class _Alloc>
+void list<_Tp, _Alloc>::_M_fill_assign(size_type __n, const _Tp& __val)
+{
+	iterator __i = begin();
+	for( ; __i != end() && __n > 0; ++__i, --__n)
+		*__i = __val;
+	if(__n > 0)
+		insert(end(), __n, __val);
+	else
+		erase(__i, end());
+}
+
+template <class _Tp, class _Alloc>
+void list<_Tp, _Alloc>::remove(const _Tp& __value)
+{
+	iterator __first = begin();
+	iterator __last = end();
+	while(__first != __last) {
+		iterator __next = __first;
+		++__next;
+		if(*__first == __value)
+			erase(__first);
+		__first = __next;
+	}
+}
+
+template <class _Tp, class _Alloc>
+void list<_Tp, _Alloc>::unique(){
+	iterator __first = begin();
+	iterator __last = end();
+	if(__first == __last)
+		return;
+	iterator __next = __first;
+	while (++__next != __last) {
+		if(*__first == *__next)
+			erase(__next);
+		else
+			__first = __next;
+		__next = __first;
+	}
+}
+
+template <class _Tp, class _Alloc>
+void list<_Tp, _Alloc>::merge(list<_Tp, _Alloc>& __x)
+{
+	iterator __first1 = begin();
+	iterator __last1 = end();
+	iterator __first2 = __x.begin();
+	iterator __last2 =  __x.end();
+	while(__first1 != __last1 && __first2 != __last2) {
+		if(*__first2 < *__first1) {
+			iterator __next = __first2;
+			transfer(__first1, __first2, ++__next);
+			__first2 = __next;
+		}
+		else
+			++__first1;
+	}
+	if(__first2 != __last2)
+		transfer(__last1, __first2, __last2);
+}
+
+inline void __List_base_reverse(_List_node_base* __p)
+{
+	_List_node_base* __tmp = __p;
+	do {
+		Jimp_STL::swap(__tmp->_M_next, __tmp->_M_prev);
+		__tmp = __tmp->_M_prev;
+	} while (__tmp != __p);
+}
+
+template <class _Tp, class _Alloc>
+inline void list<_Tp, _Alloc>::reverse()
+{
+	__List_base_reverse(this->_M_node);
+}
+
+template <class _Tp, class _Alloc>
+void list<_Tp, _Alloc>::sort()
+{
+	if(_M_node->_M_next != _M_node && _M_node->_M_next->_M_next != _M_node) {
+		list<_Tp, _Alloc> __carry;
+		list<_Tp, _Alloc> __counter[64];
+		int __fill = 0;
+		while(!empty()) {
+			__carry.splice(__carry.begin(), *this, begin());
+			int __i = 0;
+			while (__i < __fill && !__counter[__i].empty()) {
+				__counter[__i].merge(__carry);
+				__carry.swap(__counter[__i++]);
+			}
+			__carry.swap(__counter[__i]);
+			if(__i == __fill)
+				++__fill;
+		}
+
+		for(int __i = 1; __i < __fill; ++__i)
+			__counter[__i].merge(__counter[__i - 1]);
+		swap(__counter[__fill - 1]);
+	}
+}
 
 __STL_END_NAMESPACE
 
