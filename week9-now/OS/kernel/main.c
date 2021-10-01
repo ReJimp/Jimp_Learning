9be52a8a8405851626767d3f6390ab9e7bd12c0c
Jimp <2632697420@qq.com>
2021-10-02 03:00:28 +0800

    'week13'

--- week9-now/OS/kernel/main.c
+++ week9-now/OS/kernel/main.c
@@ -1,16 +1,14 @@
 #include "print.h"
 int main()
 {
-    put_char('k');
-    put_char('e');
-    put_char('r');
-    put_char('n');
-    put_char('e');
-    put_char('l');
+    put_str("Jimp will be the master of pwn\n");
+    put_int(0x12345678);
     put_char('\n');
-    put_char('1'); 
-    put_char('2'); 
-    put_char('\b'); 
-    put_char('3');
+    put_int(0xdeadbeef);
+    put_char('\n');
+    put_int(0x00000000);
+    put_char('\n');
+    put_int(0x00005678);
+    while(1);
     return 0;
 }
