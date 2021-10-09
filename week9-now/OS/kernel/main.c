3eab4b574655881b2e3e57d87dee329483843784
Jimp <2632697420@qq.com>
2021-10-09 11:31:51 +0800

    week14

--- week9-now/OS/kernel/main.c
+++ week9-now/OS/kernel/main.c
@@ -1,14 +1,10 @@
 #include "print.h"
+#include "init.h"
 int main()
 {
-    put_str("Jimp will be the master of pwn\n");
-    put_int(0x12345678);
-    put_char('\n');
-    put_int(0xdeadbeef);
-    put_char('\n');
-    put_int(0x00000000);
-    put_char('\n');
-    put_int(0x00005678);
+    put_str("I am kernel\n");
+    init_all();
+    __asm volatile("sti");
     while(1);
     return 0;
 }
