d57165584b9582211bc41375ca66babfded20e66
Jimp <2632697420@qq.com>
2021-10-12 23:38:57 +0800

    week15

--- week9-now/OS/kernel/main.c
+++ week9-now/OS/kernel/main.c
@@ -1,12 +1,20 @@
 #include "print.h"
 #include "init.h"
-#include "debug.h"
+#include "memory.h"
+// #include "debug.h"
 int main()
 {
     put_str("I am kernel\n");
     init_all();
     // __asm volatile("sti");
-    ASSERT(1==2);
+    // ASSERT(1==2);
+
+    void* addr = get_kernel_pages(3);
+    put_char('\n');
+    put_str("get_kernel_page_start_vaddr: ");
+    put_int((uint32_t)addr);
+    put_char('\n');
+
     while(1);
     return 0;
 }
